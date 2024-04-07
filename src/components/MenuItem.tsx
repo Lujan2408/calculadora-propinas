4068e2232b843c67c4d030caf8d8ddff9554add4
Jose David <lujan9495@gmail.com>
2024-04-07 15:32:13 -0500

    Agregando el signature a la función de agregar articulo con TypeScript

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -2,16 +2,16 @@ import type { MenuItem } from "../types/types"
 
 type MenuItemProps = {
     item: MenuItem
-    addItem: () => void
+    addItem: (item : MenuItem) => void
 }
 
 export default function MenuItem({ item, addItem } : MenuItemProps) {
 
     return (
-    <button className=" border-2 border-teal-400 hover:bg-teal-200 w-full p-3 flex justify-between">
-        <p className=" font-medium"
-            onClick={() => addItem()}
-        >{item.name}</p>
+    <button className=" border-2 border-teal-400 hover:bg-teal-200 w-full p-3 flex justify-between"
+        onClick={() => addItem(item)}
+    >
+        <p className=" font-medium">{item.name}</p>
         <p className=" font-black">${item.price}</p>
     </button>
   )
